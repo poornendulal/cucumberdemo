b446f05bbdcd1e5b296cd081cc52bd8a2eb3c06c
Poornendulal Venugopalan <poornendulal.venugopalan1@ibm.com>
2018-03-15 21:27:47 +0000

    this is the first commit with all files into the githut

---
+++ features/login.feature
@@ -0,0 +1,5 @@
+Feature: To demo cucumber
+Scenario: Open google and search text and verify seconds are displayed on top of the search result
+Given open the browser
+When searched anything
+Then verify seconds displayed
---
+++ src/stepdefinition/TestStep.java
@@ -0,0 +1,40 @@
+package stepdefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import framework.Automation;
+
+public class TestStep {
+	WebDriver driver;
+	@Given("^open the browser$")
+	public void open_the_browser() throws Throwable {
+		System.out.println("Setting environment properties");
+		System.setProperty("webdriver.ie.driver", "C:\\iedriver\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get("http://www.google.co.uk");
+	}
+	@When("^searched anything$")
+	public void searched_anything() throws Throwable {
+		System.out.println("Search anything performing");
+		char[] operation = { 'i', 'b'};
+		String[] control = { "lst-ib", "btnK"};
+		String[] testdata = { "this is automation test engineering demo"};
+	
+		Automation automate = new Automation();
+		automate.run(driver, operation, control, testdata);
+	}
+	@Then("^verify seconds displayed$")
+	public void verify_seconds_displayed() throws Throwable {
+		System.out.println("Performing verification");
+		char[] operation = { 'v'};
+		String[] control = { "resultStats" };
+		String[] testdata = { "seconds"};
+		Automation automate = new Automation();
+		automate.run(driver, operation, control, testdata);
+		driver.quit();
+	}
+}
