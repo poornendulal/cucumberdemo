6852b37d5ab04d56121ffdc6d0d05efb16cfd1d2
Poornendulal Venugopalan <poornendulal.venugopalan1@ibm.com>
2018-03-15 22:49:25 +0000

    updated the code to generate jason reports for jenkins

--- src/cucumberdemo/TestRunner.java
+++ src/cucumberdemo/TestRunner.java
@@ -1,11 +1,14 @@
 package cucumberdemo;
 
 import org.junit.runner.RunWith;
-
+import cucumber.api.SnippetType;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features="features",glue= {"stepdefinition"})
+@CucumberOptions(features = "features", glue = { "stepdefinition" }, snippets = SnippetType.CAMELCASE, plugin = {
+		"pretty:target/cucumber-pretty.txt", "html:target/cucumber-html-report", "json:target/cucumber.json",
+		"junit:target/cucumber-results.xml" })
 public class TestRunner {
 
 }
---
+++ target/cucumber-html-report/report.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "To demo cucumber",
+  "description": "",
+  "id": "to-demo-cucumber",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Open google and search text and verify seconds are displayed on top of the search result",
+  "description": "",
+  "id": "to-demo-cucumber;open-google-and-search-text-and-verify-seconds-are-displayed-on-top-of-the-search-result",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 3,
+  "name": "open the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "searched anything",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "verify seconds displayed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TestStep.open_the_browser()"
+});
+formatter.result({
+  "duration": 3046149071,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TestStep.searched_anything()"
+});
+formatter.result({
+  "duration": 1602284968,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TestStep.verify_seconds_displayed()"
+});
+formatter.result({
+  "duration": 39496096,
+  "status": "passed"
+});
+});
